7b26dfa1b2163c7d316ad3251707bdada06ac28f
Oswaldo Alonzo <oalonzo@gmail.com>
2021-06-28 21:18:01 -0600

    Agrengando las llaves publicas y privadas, autenticacion y conexion con la API

--- js/stripe-checkout.js
+++ js/stripe-checkout.js
@@ -1,3 +1,19 @@
+//* Obteniendo las llaves publica y privada
 import STRIPE_KEYS from "./stripe-keys.js";
 
-console.log(STRIPE_KEYS);
+//* definiendo las variables
+const d = document,
+  $tacos = d.getElementById("tacos"),
+  $template = d.getElementById("taco-template").content,
+  $fragment = d.createDocumentFragment();
+
+//* Realizando la conexion a la API de Stripe
+
+fetch("https://api.stripe.com/v1/products", {
+  headers: {
+    //Autorizacion de autenticacion con stripe
+    Authorization: `Bearer ${STRIPE_KEYS.secret}`,
+  },
+}).then((res) => {
+  console.log(res);
+});
