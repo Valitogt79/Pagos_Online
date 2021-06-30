68c19f8bd2109cf261b09f39dc18aa4b2112106b
Oswaldo Alonzo <oalonzo@gmail.com>
2021-06-29 20:16:40 -0600

    Agregando la programacion al evento click de las tarjetas.

--- js/stripe-checkout.js
+++ js/stripe-checkout.js
@@ -15,6 +15,9 @@ const d = document,
 
 let products, prices;
 
+//*dando formato a el precio.
+const moneyFormat = (num) => `Q. ${num.slice(0, -2)}.${num.slice(-2)}`;
+
 //* Realizando las peticiones a la Api de Stripe
 Promise.all([
   fetch("https://api.stripe.com/v1/products", fetchOptions),
@@ -24,11 +27,11 @@ Promise.all([
   .then((json) => {
     products = json[0].data;
     prices = json[1].data;
-    console.log(products, prices);
+    //console.log(products, prices);
 
     prices.forEach((el) => {
       let productData = products.filter((product) => product.id === el.product);
-      console.log(productData);
+      //console.log(productData);
 
       $template.querySelector(".taco").setAttribute("data-price", el.id);
       $template.querySelector("img").src = productData[0].images[0];
@@ -36,7 +39,7 @@ Promise.all([
       $template.querySelector("figcaption").innerHTML = `
       ${productData[0].name}
       <br>
-      ${el.unit_amount_decimal} ${el.currency}
+      ${moneyFormat(el.unit_amount_decimal)}
       `;
       let $clone = d.importNode($template, true);
       $fragment.appendChild($clone);
@@ -44,8 +47,29 @@ Promise.all([
     $tacos.appendChild($fragment);
   })
   .catch((err) => {
-    console.log(err);
+    //console.log(err);
     let message =
       err.statusText || "Ocurrió un error al conectase con la API de Stripe";
     $tacos.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
   });
+
+//*Programando el click
+d.addEventListener("click", (e) => {
+  if (e.target.matches(".taco *")) {
+    let price = e.target.parentElement.getAttribute("data-price");
+    // console.log(price);
+    Stripe(STRIPE_KEYS.public)
+      .redirectToCheckout({
+        lineItems: [{ price, quantity: 1 }],
+        mode: "subscription",
+        successUrl: "http://127.0.0.1:5501/Pagos_Online/assets/stripe-success.html",
+        cancelUrl: "http://127.0.0.1:5501/Pagos_Online/assets/stripe-cancel.html",
+      })
+      .then((res) => {
+        if (res.error) {
+          console.log(res);
+          $tacos.insertAdjacentHTML("afterend", res.error.message);
+        }
+      });
+  }
+});
